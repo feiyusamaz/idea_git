89e5092ac60769486ebc7c65eee76fb646426130
study <137763821@qq.com>
2019-09-14 11:45:59 +0800

    创建本地仓库并将项目添加到本地仓库中

---
+++ MySpringBootStudy01/src/main/java/com/study/controller/StudyController.java
@@ -0,0 +1,16 @@
+package com.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/study")
+public class StudyController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "大家好，才是真的好！";
+    }
+}
